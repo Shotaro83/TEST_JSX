a24160a7a85afb9f69a46b3d7ba7c9f278f99ae6
Shotaro83 <hisamatsu@83design.jp>
2023-12-07 18:53:06 +0900

    reduxのテスト

--- 83design-test/src/App.jsx
+++ 83design-test/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount, dicrementByAmount } from "./redux/counterSlice";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+  const [dicreamentAmount, setDicreamentAmount] = useState("2")
+  const incre = () => dispatch(increment())
+  const decre = () => dispatch(decrement())
+  const increBy = () => dispatch(incrementByAmount(Number(incrementAmount)))
+  const dicreBy = () => dispatch(dicrementByAmount(Number(dicreamentAmount)))
+
+ 
 
   return (
     <>
-      Hello World 83
+      <h1>Count: {count}</h1>
+      <input
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <button onClick={incre}>＋</button>
+      <button onClick={decre}>ー</button>
+      <button onClick={dicreBy}>追加</button>
     </>
   )
 }
