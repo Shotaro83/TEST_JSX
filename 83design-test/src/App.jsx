a56d1389d88859c773a36c1c17c4e73d8d725a81
Shotaro83 <hisamatsu@83design.jp>
2023-12-14 16:17:39 +0900

    tsのお勉強がしたい!

--- 83design-test/src/App.jsx
+++ 83design-test/src/App.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, incrementByAmount, dicrementByAmount } from "./redux/counterSlice";
+import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState("2");
-  const [dicreamentAmount, setDicreamentAmount] = useState("2")
-  const incre = () => dispatch(increment())
-  const decre = () => dispatch(decrement())
-  const increBy = () => dispatch(incrementByAmount(Number(incrementAmount)))
-  const dicreBy = () => dispatch(dicrementByAmount(Number(dicreamentAmount)))
+  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("")
+  const RDcount = useSelector((state) => state.counter.count);
 
- 
+  const showMessage = () =>{
+    if(message == ""){
+      setMessage("Hello 83Design")
+    }
+    else{
+      setMessage("")
+    }
+  }
 
   return (
     <>
-      <h1>Count: {count}</h1>
-      <input
-        value={incrementAmount}
-        onChange={(e) => setIncrementAmount(e.target.value)}
-      />
-      <button onClick={incre}>＋</button>
-      <button onClick={decre}>ー</button>
-      <button onClick={dicreBy}>追加</button>
+      <button onClick={()=>showMessage()}>
+        push!
+      </button>
+      <input type="date" value="2017-06-01" style={{fontFamily:"sans-serif", border:"none"}}/>
+      <p>
+        {message}
+        {console.log(RDcount)}
+      </p>
     </>
   )
 }
